61b952ec71b622e2af67cbd6ae7bb5148dcd7efe
ManavR123 <manav.rathod@berkeley.edu>
2022-04-18 22:17:40 -0700

    finalize split logic

--- pseudowords/split_dataset.py
+++ pseudowords/split_dataset.py
@@ -1,6 +1,7 @@
 import argparse
-import random
 import json
+import random
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
@@ -8,34 +9,42 @@ from sklearn.model_selection import train_test_split
 def main(args):
     random.seed(args.seed)
     diction = json.load(open(args.dictionary, "r"))
-    terms = pd.read_csv(args.terms, sep="\t")
     dataset = pd.read_csv(args.dataset)
 
-    unseen_terms = set(random.choices(terms["term"], k=int(len(terms) * 0.05)))
-    unseen_pseudowords = random.sample(diction.keys(), k=int(0.1 * len(diction)))
-    for pseudoword in unseen_pseudowords:
-        unseen_terms.update(diction[pseudoword])
+    pseudoword_to_num_senses = [{"pseudoword": k, "num_senses": len(v)} for k, v in diction.items()]
+    df = pd.DataFrame(pseudoword_to_num_senses)
+    unseen_pseudowords = set(
+        df.groupby("num_senses", group_keys=False)
+        .apply(lambda x: x.sample(int(0.1 * len(diction) // 7)))["pseudoword"]
+        .to_list()
+    )
 
-    percent = len(unseen_terms) / len(terms) * 100
-    print(f"Hidding {percent:.2f}% of terms")
+    mask = dataset["acronym"].isin(unseen_pseudowords)
+    train_1 = dataset[~mask]
+    test_new_pseudowords = dataset[mask]
 
-    mask = dataset["expansion"].isin(unseen_terms)
-    train = dataset[~mask]
-    test_1 = dataset[mask]
+    seen_pseudowords = set(diction.keys()) - unseen_pseudowords
+    senses = []
+    for pseudoword in seen_pseudowords:
+        senses.extend(diction[pseudoword])
+    unseen_senses = set(random.sample(senses, k=int(0.1 * len(senses))))
 
-    train, test_2 = train_test_split(train, test_size=0.1, random_state=42)
-    test = pd.concat([test_1, test_2])
+    mask = train_1["expansion"].isin(unseen_senses)
+    train_2 = train_1[~mask]
+    test_new_senses = train_1[mask]
 
-    print(f"Train: {len(train)}, Test: {len(test)}")
+    train, test_not_new = train_test_split(train_2, test_size=0.1, random_state=42)
+
     train.to_csv("pseudowords/pseudowords_train.csv", index=False)
-    test.to_csv("pseudowords/pseudowords_test.csv", index=False)
+    test_new_pseudowords.to_csv("pseudowords/pseudowords_test_new_pseudowords.csv", index=False)
+    test_new_senses.to_csv("pseudowords/pseudowords_test_new_senses.csv", index=False)
+    test_not_new.to_csv("pseudowords/pseudowords_test_not_new.csv", index=False)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dictionary", type=str, default="pseudowords/pseudoword_kNN_expansions.json")
+    parser.add_argument("--dictionary", type=str, default="pseudowords/pseudoword_kNN_0.95_expansions.json")
     parser.add_argument("--dataset", type=str, default="pseudowords/pseudowords_dataset.csv")
-    parser.add_argument("--terms", type=str, default="pseudowords/terms.tsv")
     parser.add_argument("--seed", type=int, default=42)
     args = parser.parse_args()
     main(args)
