2e684870acb0932ea917985a7d46282416d2a8b1
ManavR123 <manav.rathod@berkeley.edu>
2022-04-25 16:59:48 -0700

    updated freq hist code

--- wikipedia_parsing/sense_frequency_hist.py
+++ wikipedia_parsing/sense_frequency_hist.py
@@ -6,9 +6,12 @@ import numpy as np
 
 terms = json.load(open("wikipedia_parsing/ambiguous_term_to_senses.json"))
 data = [len(term) for term in terms.values()]
-plt.hist(data, bins=np.arange(1, max(data) + 2))
+arr = plt.hist(data, bins=np.arange(2, 9))
 plt.ylabel("Frequency")
 plt.xlabel("Number of Senses")
-plt.xticks(np.arange(1, max(data) + 2))
+plt.xticks(np.arange(2, 9))
 plt.title(f"Sense Frequencies for Ambiguous Wikipedia Terms (n={len(terms)})")
+for i in range(0, 6):
+    plt.text(arr[1][i] + 0.25, arr[0][i], str(int(arr[0][i])))
+
 plt.savefig("wikipedia_parsing/ambiguous_terms_sense_frequency_distribution.png")
